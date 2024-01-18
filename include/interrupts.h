26abf19e1ef9f6d7522aeba113cac082b688d0d6
Worcnaz Prado <108674581+Worcnaz-Kyouma@users.noreply.github.com>
2024-01-18 00:18:05 -0300

    feat: interruptsstubs and interrepts.h implemented(i think so)

--- include/interrupts.h
+++ include/interrupts.h
@@ -2,19 +2,37 @@
 #define __INTERRUPTS_H
 
 #include "types.h"
+#include "gdt.h"
 
     class InterruptManager{
         protected:
 
             struct GateDescriptor{
                 uint16_t handlerAddressLowBits;
-                uint16_t han
+                uint16_t gdt_codeSegmentSelector;
+                uint8_t reserved;
+                uint8_t access;
+                uint16_t handlerAddressHighBits;
 
             } __attribute__((packed));
 
+            static GateDescriptor interruptDescriptorTable[256];
+
+            static void SetInterruptDescriptorTableEntry(
+                uint8_t interruptNumber,
+                uint16_t gdt_codeSegmentSelectorOffset,
+                void (*handler)(),
+                uint8_t DescriptorPrivilegeLevel,
+                uint8_t DescriptorType
+            );
+
         public:
+            InterruptManager(GlobalDescriptorTable* gdt);
+            ~InterruptManager();
+
             static uint32_t handleInterrupt(uint8_t interruptNumber, uint32_t esp);
 
+            static void IgnoreInterruptRequest();
             static void handleInterruptRequest0x00(); //Timer interrupt?
             static void handleInterruptRequest0x01();
     };
